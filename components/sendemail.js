c7d7addf3bb492f9036804f9ad2768b91ba7a47f
ExCaliBur2020 <yashraut361@gmail.com>
2022-01-31 21:38:34 +0530

    email service added

---
+++ components/sendemail.js
@@ -0,0 +1,28 @@
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
+const sendemail = () => {
+    const form = useRef();
+    const sendEmail = (e) => {
+        e.preventDefault();
+        emailjs.sendForm('service_hz1c304', 'template_w0tkaep', form.current, 'user_7gisq653kSdNlR2N8T5P6')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+    };
+
+    return (
+        <form ref={form} onSubmit={sendEmail}>
+            <label>Name</label>
+            <input type="text" name="user_name" />
+            <label>Email</label>
+            <input type="email" name="user_email" />
+            <label>Message</label>
+            <textarea name="message" />
+            <input type="submit" value="Send" />
+        </form>
+    );
+}
+export default sendemail;
