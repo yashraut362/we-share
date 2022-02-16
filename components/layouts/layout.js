61d24d9133c479c24ef378975c5171304673f20b
Yash Raut <yash@koyaltechsolutions.com>
2022-02-16 21:51:22 +0530

    added bg

--- components/layouts/layout.js
+++ components/layouts/layout.js
@@ -2,10 +2,11 @@ import Navbar from "../navbar";
 
 const Layout = ({ children }) => {
   return (
-    <div className="w-full h-screen overflow-y-hidden bg-white">
+    <div className="w-full h-screen overflow-y-hidden bg-orange-100 ">
       <Navbar></Navbar>
       <div className="flex h-full">
-        <div className="w-full">{children}</div>
+        <div className="w-1/3">{children}</div>
+        <div className="w-2/3  bg-[url('../public/assets/images/Team.png')]"></div>
       </div>
     </div>
   );
